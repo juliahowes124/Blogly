6b06ae6a628b213bac5c4a26d0555e78dad597e5
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2021-03-10 12:21:32 -0800

    basic user model and view

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "this-is-secret"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
 
 connect_db(app)
 db.create_all()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
