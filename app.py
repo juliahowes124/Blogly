4436f6e0c45390cd8bebd50fab1351de7ccf12e9
Jessica <jessica@Jessicas-MacBook-Pro.local>
2021-03-11 11:34:42 -0800

    add new post form

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, render_template, redirect, request
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "this-is-secret"
@@ -23,15 +23,18 @@ def index():
 
 @app.route('/users')
 def users():
+    """ Home page, lists all existing users """
     users = User.query.all()
     return render_template('index.html', users = users)
 
 @app.route('/users/new')
 def users_new():
+    """ Page to add new user"""
     return render_template('create_user.html')
 
 @app.route('/users/new', methods=['POST'])
 def add_new_user():
+    """ Form post to add new user """
     first = request.form["first"]
     last = request.form["last"]
     image = request.form["image"]
@@ -48,18 +51,20 @@ def add_new_user():
 
 @app.route('/users/<int:id>')
 def user_profile(id):
+    """ Individual user page """
     user = User.query.get(id)
 
     return render_template('existing_user.html', user=user)
 
 @app.route('/users/<int:id>/edit')
 def edit_user(id):
-
+    """ Edit individual user page """
     user = User.query.get(id)
     return render_template('/edit_form.html', user=user)
 
 @app.route('/users/<int:id>/edit', methods=['POST'])
 def save_edit_user(id):
+    """ Form post to edit individual user page """
     user = User.query.get(id)
 
     user.first_name = request.form["first"]
@@ -73,7 +78,28 @@ def save_edit_user(id):
 
 @app.route('/users/<int:id>/delete', methods=["POST"])
 def delete_user(id):
+    """ Form post to delete a user """
     user = User.query.get(id)
     db.session.delete(user)
     db.session.commit()
     return redirect('/users')
+
+@app.route('/users/<int:id>/posts/new')
+def new_post_form(id):
+    """ Page for individual user to add new post """
+    user = User.query.get(id)
+    return render_template("new_post.html", user=user)
+
+
+@app.route('/users/<int:id>/posts/new', methods=['POST'])
+def submit_new_post(id):
+    """ Form post for adding new post for individual user """
+    title = request.form["title"]
+    content = request.form["content"]
+
+    new_post = Post(title=title, content=content, author_id=id)
+
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect(f"/users/{id}")
--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -12,6 +13,7 @@ def connect_db(app):
 """Models for Blogly."""
 
 class User(db.Model):
+    """ Model for User """
 
     __tablename__ = "users"
 
@@ -25,7 +27,28 @@ class User(db.Model):
 
     image_url = db.Column(db.String(), default="https://winaero.com/blog/wp-content/uploads/2015/05/windows-10-user-account-login-icon.png")
 
+    posts = db.relationship('Post')
+
     @classmethod
     def find_user_id(cls, first, last):
         return cls.query.filter(User.first_name == first, User.last_name == last).first().id
-    
+
+class Post(db.Model):
+    """ Model for Post """
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer,
+                primary_key=True,
+                autoincrement=True)
+
+    title = db.Column(db.String(50), nullable=False)
+
+    content = db.Column(db.Text, nullable=False)
+
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user = db.relationship('User')
+
