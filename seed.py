0f36f83f2ec85872f5675b531805060df9cff73a
Jessica <jessica@Jessicas-MacBook-Pro.local>
2021-03-11 12:11:35 -0800

    add posts to seed file, fix text typo in post details template

--- seed.py
+++ seed.py
@@ -1,4 +1,4 @@
-from models import User, db
+from models import User, Post, db
 from app import app
 
 db.drop_all()
@@ -10,8 +10,16 @@ user1 = User(first_name='Alan', last_name='Alda')
 user2 = User(first_name='Joel', last_name='Burton', image_url='http://joelburton.com/joel-burton.jpg')
 user3 = User(first_name='Jane', last_name='Smith')
 
+post_alan1 = Post(title="Hello", content="My name is Alan Alda", author_id=1)
+post_alan2 = Post(title="I am an actor", content="I was in M*A*S*H, which is an old show", author_id=1)
+post_joel = Post(title="Introduction to Typescript", content="Typescript is like Javascript", author_id=2)
+
 db.session.add(user1)
 db.session.add(user2)
 db.session.add(user3)
+
+db.session.add(post_alan1)
+db.session.add(post_alan2)
+db.session.add(post_joel)
 
 db.session.commit()
